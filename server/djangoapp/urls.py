4046088f9181398272313950425d4e4193278b46
jguiue <jguiue@gmail.com>
2022-11-25 12:34:41 -0500

    Here we go

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -6,23 +6,23 @@ from . import views
 app_name = 'djangoapp'
 urlpatterns = [
     # route is a string contains a URL pattern
-    # view refers to the view function
-    # name the URL
 
-    # path for about view
+from django.urls import path
+from django.conf.urls.static import static
+from django.conf import settings
+from . import views
 
-    # path for contact us view
-
-    # path for registration
-
-    # path for login
-
-    # path for logout
-
+app_name = 'djangoapp'
+urlpatterns = [
+    # route is a string contains a URL pattern
     path(route='', view=views.get_dealerships, name='index'),
-
-    # path for dealer reviews view
-
-    # path for add a review view
+    path('dealer/<int:id>/', views.get_dealer_details, name='dealer_details'),
+    path('dealer/<int:id>/review', views.add_review, name='add_review'),
+    path(route='about/', view=views.about, name='about'),
+    path(route='contact/', view=views.contact, name='contact'),
+    path('registration/', views.registration_request, name='registration'),
+    path('login/', views.login_request, name='login'),
+    path('logout/', views.logout_request, name='logout'),
+    path(route='', view=views.get_dealerships, name='index'),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
