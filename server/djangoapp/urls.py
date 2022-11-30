bcd7b267febf0068c6fb86ff451412562a1faa62
jguiue <jguiue@gmail.com>
2022-11-30 07:40:01 -0500

    Here we go

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -11,7 +11,7 @@ urlpatterns = [
     # name the URL
 
     # path for about view
-    path(route='about/',view=views.about,name='about')
+    path(route='about/',view=views.about,name='about'),
 
     # path for contact us view
     path(route='contact/', view=views.contact, name='contact'),
