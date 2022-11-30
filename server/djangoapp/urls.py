1d3c8a5f9a7b9c8b14ad83c7818dd4ee51c1c91a
jguiue <jguiue@gmail.com>
2022-11-30 07:19:53 -0500

    Here we go

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -5,24 +5,24 @@ from . import views
 
 app_name = 'djangoapp'
 urlpatterns = [
-    # route is a string contains a URL pattern
+  
+   
+    # view refers to the view function
+    # name the URL
 
-from django.urls import path
-from django.conf.urls.static import static
-from django.conf import settings
-from . import views
+    # path for about view
+    path(route='about/',view=views.about,name='about')
 
-app_name = 'djangoapp'
-urlpatterns = [
-    # route is a string contains a URL pattern
-    path(route='', view=views.get_dealerships, name='index'),
-    path('dealer/<int:id>/', views.get_dealer_details, name='dealer_details'),
-    path('dealer/<int:id>/review', views.add_review, name='add_review'),
-    path(route='about/', view=views.about, name='about'),
+    # path for contact us view
     path(route='contact/', view=views.contact, name='contact'),
-    path('registration/', views.registration_request, name='registration'),
-    path('login/', views.login_request, name='login'),
-    path('logout/', views.logout_request, name='logout'),
-    path(route='', view=views.get_dealerships, name='index'),
 
+    # path for registration
+
+    # path for login
+
+    # path for logout
+    path(route='', view=views.get_dealerships, name='index'),
+    # path for dealer reviews view
+
+    # path for add a review view
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
